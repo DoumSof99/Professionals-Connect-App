d3458195113fb25099d95d26a7ebe8b45de91d56
DoumSof99 <sofiadmn99@gmail.com>
2024-05-28 12:42:41 +0300

    Adding a token service

---
+++ ProfessionalsConnectApp/API/Services/TokenService.cs
@@ -0,0 +1,10 @@
+﻿using API.Entities;
+using API.Interfaces;
+
+namespace API.Services {
+    public class TokenService : ITokenService {
+        public string GreateToken(AppUser user) {
+            throw new NotImplementedException();
+        }
+    }
+}
