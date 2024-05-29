02af67578214339e90b70f69996eca488ee569e7
DoumSof99 <sofiadmn99@gmail.com>
2024-05-29 10:47:28 +0300

    Adding the createToken logic

--- ProfessionalsConnectApp/API/Services/TokenService.cs
+++ ProfessionalsConnectApp/API/Services/TokenService.cs
@@ -1,10 +1,38 @@
 ﻿using API.Entities;
 using API.Interfaces;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace API.Services {
     public class TokenService : ITokenService {
-        public string GreateToken(AppUser user) {
-            throw new NotImplementedException();
+        
+        private readonly SymmetricSecurityKey _key;
+
+        public TokenService(IConfiguration config)
+        {
+            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
         }
-    }
+
+        public string GreateToken(AppUser user) {
+
+            var claims = new List<Claim>() {
+                new Claim(JwtRegisteredClaimNames.NameId, user.UserName)
+            };
+
+            var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(7),
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+    } 
 }
