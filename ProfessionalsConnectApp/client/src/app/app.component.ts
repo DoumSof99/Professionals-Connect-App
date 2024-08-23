0909ba69278e4c64271b9dce00614f980943080a
DoumSof99 <sofiadmn99@gmail.com>
2024-08-23 19:23:54 +0300

    Parent to child communication with new approach of angular 17+

--- ProfessionalsConnectApp/client/src/app/app.component.ts
+++ ProfessionalsConnectApp/client/src/app/app.component.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from "./nav/nav.component";
@@ -12,15 +11,10 @@ import { HomeComponent } from "./home/home.component";
   styleUrl: './app.component.css',
   imports: [RouterOutlet, NavComponent, HomeComponent]
 })
-export class AppComponent implements OnInit{
-  
-  http = inject(HttpClient);
+export class AppComponent implements OnInit{  
   private accountService = inject(AccountService);
-  title = 'Professional Connect App';
-  users: any;
   
   ngOnInit(): void {
-    this.getUsers();    
     this.setCurrentUser();
   }
 
@@ -30,13 +24,4 @@ export class AppComponent implements OnInit{
     const user = JSON.parse(userString);
     this.accountService.currentUser.set(user);
   }
-  
-  getUsers(){
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete: () => console.log('Request completed')
-    });
-  }
-
 }
--- ProfessionalsConnectApp/client/src/app/register/register.component.ts
+++ ProfessionalsConnectApp/client/src/app/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -9,6 +9,7 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  usersFormHomeComponent = input.required<any>()
   model: any = {};
 
   register(){
