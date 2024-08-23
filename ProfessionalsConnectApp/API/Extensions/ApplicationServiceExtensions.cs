e92935ba42dee3008611ca87a41d2c136b73dfb0
DoumSof99 <sofiadmn99@gmail.com>
2024-08-23 11:13:52 +0300

    Added extension methods to the IServiceCollection

---
+++ ProfessionalsConnectApp/API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Text;
+using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extensions;
+
+public static class ApplicationServiceExtensions 
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config) {
+        services.AddControllers();
+        services.AddDbContext<DataContext>(opti => {
+            opti.UseSqlite(config.GetConnectionString("DefaultConnections"));
+        });
+        services.AddCors();
+        services.AddScoped<ITokenService, TokenService>();
+        
+        return services;
+    }
+
+}
