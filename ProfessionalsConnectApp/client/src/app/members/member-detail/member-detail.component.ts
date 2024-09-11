1557a95a28ed821e5948656481038b794e66d53c
DoumSof99 <sofiadmn99@gmail.com>
2024-09-11 21:25:11 +0300

    Routing from member-list to the member-card

--- ProfessionalsConnectApp/client/src/app/members/member-detail/member-detail.component.ts
+++ ProfessionalsConnectApp/client/src/app/members/member-detail/member-detail.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { MemberService } from '../../_services/member.service';
+import { ActivatedRoute } from '@angular/router';
+import { Member } from '../../_models/member';
 
 @Component({
   selector: 'app-member-detail',
@@ -7,6 +10,20 @@ import { Component } from '@angular/core';
   templateUrl: './member-detail.component.html',
   styleUrl: './member-detail.component.css'
 })
-export class MemberDetailComponent {
+export class MemberDetailComponent implements OnInit{
+  private memberService = inject(MemberService);
+  private routes = inject(ActivatedRoute);
+  member?: Member;
 
+  ngOnInit(): void {
+    this.loadMember();
+  }
+
+  loadMember(){
+    const username = this.routes.snapshot.paramMap.get('username');
+    if(!username) return;
+    this.memberService.getMember(username).subscribe({
+      next: member => this.member = member
+    })
+  }
 }
