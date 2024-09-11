4084f01131dfff5c5dd64ee1c260b47bbfc9d5f7
DoumSof99 <sofiadmn99@gmail.com>
2024-09-11 21:14:19 +0300

    Created new member-card component, added styling and used interceptor to send the token

--- ProfessionalsConnectApp/client/src/app/_services/member.service.ts
+++ ProfessionalsConnectApp/client/src/app/_services/member.service.ts
@@ -2,7 +2,6 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { Member } from '../_models/member';
-import { AccountService } from './account.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,22 +9,14 @@ import { AccountService } from './account.service';
 export class MemberService {
 
   private http = inject(HttpClient);
-  private accountService = inject(AccountService);
   baseUrl = environment.apiUrl;
 
   getMembers(){
-    return this.http.get<Member[]>(this.baseUrl + 'users', this.getHttpOptions())
+    return this.http.get<Member[]>(this.baseUrl + 'users');
   }
 
   getMember(username: string){
-    return this.http.get<Member>(this.baseUrl + 'users/' + username, this.getHttpOptions());
+    return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
 
-  getHttpOptions(){
-    return {
-      headers: new HttpHeaders({
-        Authorization: `Bearer ${this.accountService.currentUser()?.token}`
-      })
-    }
-  }
 }
