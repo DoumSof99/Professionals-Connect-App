f2fe15b971a3045d93b01da202ab30560833526d
DoumSof99 <sofiadmn99@gmail.com>
2024-10-08 20:48:42 +0300

    Added sorting functionality on the client

--- ProfessionalsConnectApp/client/src/app/_models/userParams.ts
+++ ProfessionalsConnectApp/client/src/app/_models/userParams.ts
@@ -6,6 +6,7 @@ export class UserParams{
     maxAge = 90;
     pageNumber = 1;
     pageSize = 5;
+    orderBy = 'lastActive';
 
     constructor(user: User | null){
         this.gender = user?.gender === 'female' ? 'male' : 'female';
