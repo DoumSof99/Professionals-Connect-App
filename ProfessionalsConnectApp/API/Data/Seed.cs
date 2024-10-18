bde9ccc602d90a1d7c805d12e8972341ce064698
DoumSof99 <sofiadmn99@gmail.com>
2024-10-18 16:45:40 +0300

    added roles in the db

--- ProfessionalsConnectApp/API/Data/Seed.cs
+++ ProfessionalsConnectApp/API/Data/Seed.cs
@@ -8,7 +8,7 @@ using Microsoft.EntityFrameworkCore;
 namespace API.Data;
 
 public class Seed{
-    public static async Task SeedUsers(UserManager<AppUser> userManager){
+    public static async Task SeedUsers(UserManager<AppUser> userManager, RoleManager<AppRole> roleManager){
         if(await userManager.Users.AnyAsync()) return;
 
         var userData = await File.ReadAllTextAsync("Data/UserDataSeed.json");
@@ -17,10 +17,33 @@ public class Seed{
         var users = JsonSerializer.Deserialize<List<AppUser>>(userData, options);
         if(users == null) return;
 
+        var roles = new List<AppRole> {
+            new() { Name = "Member" },
+            new() { Name = "Admin" },
+            new() { Name = "Moderator" },
+        };
+
+        foreach (var role in roles)
+        {
+            await roleManager.CreateAsync(role);
+        }
+
         foreach (var user in users)
         {                    
             user.UserName = user.UserName!.ToLower();
             await userManager.CreateAsync(user, "Pa$$w0rd");
+            await userManager.AddToRoleAsync(user, "Member");
         }
+
+        var admin = new AppUser{
+            UserName = "admin",
+            KnownAs = "Admin",
+            Gender = "",
+            City = "",
+            Country = ""
+        };
+
+        await userManager.CreateAsync(admin, "Pa$$w0rd");
+        await userManager.AddToRolesAsync(admin, ["Admin", "Moderator"]);
     }
 }
