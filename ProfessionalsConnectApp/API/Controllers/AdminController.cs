8eaf83ff9eacb0bcc6d21851bab1b5c055798416
DoumSof99 <sofiadmn99@gmail.com>
2024-10-18 17:11:17 +0300

    Created the GetUsersWithRoles in admin controller

--- ProfessionalsConnectApp/API/Controllers/AdminController.cs
+++ ProfessionalsConnectApp/API/Controllers/AdminController.cs
@@ -1,15 +1,27 @@
 using System;
+using API.Entities;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-public class AdminController : BaseApiController
+public class AdminController(UserManager<AppUser> userManager) : BaseApiController
 {
     [Authorize(Policy = "RequireAdminRole")]
     [HttpGet("users-with-roles")]
-    public ActionResult GetUsersWothRoles(){
-        return Ok("Only admin can see this");
+    public async Task<ActionResult> GetUsersWithRoles(){
+        
+        var users = await userManager.Users
+            .OrderBy(x => x.UserName)
+            .Select(x => new {
+                x.Id,
+                Username = x.UserName,
+                Roles = x.UserRoles.Select(r => r.Role.Name).ToList()
+            }).ToListAsync();
+
+        return Ok(users);
     }
 
     [Authorize(Policy = "ModeratePhotoRole")]
