a938128422e21054cf1a4003732c44b635a33ab9
DoumSof99 <sofiadmn99@gmail.com>
2024-10-18 16:37:12 +0300

    updated the AccountController to user the UserManager from Identity

--- ProfessionalsConnectApp/API/Data/Seed.cs
+++ ProfessionalsConnectApp/API/Data/Seed.cs
@@ -19,6 +19,7 @@ public class Seed{
 
         foreach (var user in users)
         {                    
+            user.UserName = user.UserName!.ToLower();
             await userManager.CreateAsync(user, "Pa$$w0rd");
         }
     }
