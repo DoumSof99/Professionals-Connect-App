9c8b2567f47fa5008f9b4e7dd2d1ae7897c1258d
DoumSof99 <sofiadmn99@gmail.com>
2024-10-18 16:24:47 +0300

    updated the database to has the identity's schema

--- ProfessionalsConnectApp/API/Data/Seed.cs
+++ ProfessionalsConnectApp/API/Data/Seed.cs
@@ -2,26 +2,24 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Text.Json;
 using API.Entities;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data;
 
 public class Seed{
-    public static async Task SeedUsers(DataContext context){
-        if(await context.Users.AnyAsync()) return;
+    public static async Task SeedUsers(UserManager<AppUser> userManager){
+        if(await userManager.Users.AnyAsync()) return;
 
         var userData = await File.ReadAllTextAsync("Data/UserDataSeed.json");
         var options = new JsonSerializerOptions{ PropertyNameCaseInsensitive = true};
+
         var users = JsonSerializer.Deserialize<List<AppUser>>(userData, options);
         if(users == null) return;
 
         foreach (var user in users)
-        {
-            using var hmac = new HMACSHA512();                        
-            context.Users.Add(user);
+        {                    
+            await userManager.CreateAsync(user, "Pa$$w0rd");
         }
-
-        await context.SaveChangesAsync();
-
     }
 }
