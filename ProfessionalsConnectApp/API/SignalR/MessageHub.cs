028085d418dec7433faaf8342d3e1adbf87d41fe
DoumSof99 <sofiadmn99@gmail.com>
2024-10-21 15:21:10 +0300

    Created MessageHub class with SignalR, server side

---
+++ ProfessionalsConnectApp/API/SignalR/MessageHub.cs
@@ -0,0 +1,32 @@
+using System;
+using API.Extensions;
+using API.Interfaces;
+using Microsoft.AspNetCore.SignalR;
+
+namespace API.SignalR;
+
+public class MessageHub(IMessageRespository messageRespository) : Hub
+{
+    public override async Task OnConnectedAsync()
+    {
+        var httpContext = Context.GetHttpContext();
+        var otherUser = httpContext?.Request.Query["user"];
+
+        if(Context.User == null || string.IsNullOrEmpty(otherUser)) throw new Exception("Cannot join group");
+        var groupName = GetGroupName(Context.User.GetUsername(), otherUser);
+        await Groups.AddToGroupAsync(Context.ConnectionId, groupName);        
+
+        var messages = await messageRespository.GetMessageThread(Context.User.GetUsername(), otherUser!);
+        await Clients.Group(groupName).SendAsync("RecievedMessageThread", messages);
+    }
+
+    public override Task OnDisconnectedAsync(Exception? exception)
+    {
+        return base.OnDisconnectedAsync(exception);
+    }
+
+    private string GetGroupName(string caller, string? other){
+        var stringCompare = string.CompareOrdinal(caller, other) < 0;
+        return stringCompare ? $"{caller}-{other}" : $"{other}-{caller}";
+    }
+}
