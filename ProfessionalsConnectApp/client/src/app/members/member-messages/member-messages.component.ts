6eccc4733bd32dab15187e4b6b7de96771dea5aa
DoumSof99 <sofiadmn99@gmail.com>
2025-02-11 09:58:49 +0200

    Changes for messages and add angular commands in yml workflow

--- ProfessionalsConnectApp/client/src/app/members/member-messages/member-messages.component.ts
+++ ProfessionalsConnectApp/client/src/app/members/member-messages/member-messages.component.ts
@@ -16,16 +16,18 @@ export class MemberMessagesComponent implements AfterViewChecked {
   messageService = inject(MessageService);
   username = input.required<string>();
   messageContent = '';
+  loading = false;
 
   ngAfterViewChecked(): void {
     this.scrollToBottom();
   }
 
   sendMessage(){
+    this.loading = true;
     this.messageService.sendMessage(this.username(), this.messageContent).then(() => {
       this.messageForm?.reset();
       this.scrollToBottom();
-    });
+    }).finally(() => this.loading = false);
   }
 
   private scrollToBottom(){
